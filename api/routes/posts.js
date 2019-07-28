e4bd7827d22e02bf33099035eade86a9ad653615
sounak07 <sounakume@gmail.com>
2019-07-29 01:40:46 +0530

    Get post routes added

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,12 +6,42 @@ const passport = require("passport");
 const Post = require("../../models/Post");
 const validationPostInput = require("../../validation/postValidation");
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Post Router"
-  });
+//GET ALL POSTS
+//PUBLIC ROUTE
+router.get("/all", (req, res) => {
+  let errors = {};
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(e => {
+      errors.noposts = "No posts found";
+      res.status(404).json();
+    });
 });
 
+//GET POSTS BY ID
+//PUBLIC ROUTE
+router.get("/post/:post_id", (req, res) => {
+  let errors = {};
+  Post.findById(req.params.post_id)
+    .then(post => {
+      if (!post) {
+        errors.nopost = "No post found with that id";
+        res.status(404).json(errors);
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(e => {
+      errors.nopost = "Not found";
+      res.status(404).json();
+    });
+});
+
+//POST POSTS
+//PRIVATE ROUTE
 router.post(
   "/addPost",
   passport.authenticate("jwt", { session: false }),
