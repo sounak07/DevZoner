2b21bdf582d72ecde31429e56f21dad3ab7ea4b3
sounak07 <sounakume@gmail.com>
2019-07-30 11:32:01 +0530

    Client setup done

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use("/api/user", user);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
