1cf5f2ded2d37a90c310b9934ac53dcff3f90615
sounak07 <sounakume@gmail.com>
2019-08-02 16:28:01 +0530

    Github part done

--- client/src/components/profile/profileGithub.js
+++ client/src/components/profile/profileGithub.js
@@ -1,29 +1,64 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ProfileGithub extends Component {
+  state = {
+    clientId: "341412a19f8108974369",
+    clientSecret: "312ca4fdb3ff09cbcb8e9566eddb74f562918005",
+    count: 5,
+    sort: "created: asc",
+    repos: []
+  };
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { count, sort, clientId, clientSecret } = this.state;
+
+    fetch(
+      `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (this.refs.myRef) {
+          this.setState({ repos: data });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { repos } = this.state;
+
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>
+              <Link to={repo.html_url} className="text-info" target="_blank">
+                {repo.name}
+              </Link>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <div className="col-md-6">
+            <span className="badge badge-info mr-1">
+              Stars: {repo.stargazers_count}
+            </span>
+            <span className="badge badge-secondary mr-1">
+              Watchers: {repo.watchers_count}
+            </span>
+            <span className="badge badge-success">
+              Forks: {repo.forks_count}
+            </span>
+          </div>
+        </div>
+      </div>
+    ));
     return (
       <div ref="myRef">
         <hr />
-        {/* <h3 className="mb-4">Latest Github Repos</h3>
-        <div key={repo.id} className="card card-body mb-2">
-          <div className="row">
-            <div className="col-md-6">
-              <h4>
-                <Link to={repo.html_url} className="text-info" target="_blank">
-                  {" "}
-                  Repository One
-                </Link>
-              </h4>
-              <p>Repository description</p>
-            </div>
-            <div className="col-md-6">
-              <span className="badge badge-info mr-1">Stars: 44</span>
-              <span className="badge badge-secondary mr-1">Watchers: 21</span>
-              <span className="badge badge-success">Forks: 122</span>
-            </div>
-          </div> */}
-        {/* </div> */}
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repoItems}
       </div>
     );
   }
