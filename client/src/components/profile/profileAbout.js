2e7f1adcd84fb775250599a4370ed8782f7cf999
sounak07 <sounakume@gmail.com>
2019-08-02 14:36:05 +0530

    about done

--- client/src/components/profile/profileAbout.js
+++ client/src/components/profile/profileAbout.js
@@ -2,36 +2,23 @@ import React, { Component } from "react";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+    const fName = profile.user.name.trim().split(" ")[0];
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">John's Bio</h3>
-            <p className="lead">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Provident fuga cum necessitatibus blanditiis vel, officia facere
-              porro esse numquam assumenda doloremque saepe aliquam nemo
-              excepturi aliquid maiores! Excepturi, libero repudiandae.
-            </p>
+            <h3 className="text-center text-info">{fName}'s Bio</h3>
+            <p className="lead">{profile.bio}</p>
             <hr />
             <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
               <div className="d-flex flex-wrap justify-content-center align-items-center">
-                <div className="p-3">
-                  <i className="fa fa-check" /> HTML
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> CSS
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> JavaScript
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> Python
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> C#
-                </div>
+                {profile.skills.map((sk, index) => (
+                  <div key={index} className="p-3">
+                    <i className="fa fa-check" /> {sk}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
