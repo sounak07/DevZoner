23c20ef65e9484ea976c9e87b7f396a63f5d00ad
sounak07 <sounakume@gmail.com>
2019-08-09 13:52:19 +0530

    Notify ui minor code changes

--- client/src/components/notifications/NotifyBox.js
+++ client/src/components/notifications/NotifyBox.js
@@ -4,14 +4,14 @@ import "./NotifyBox.css";
 class NotifyBox extends Component {
   render() {
     return (
-      <div class="popover__wrapper">
-        <div class="popover__content">
-          <ul class="list-group">
-            <li class="list-group-item">Cras justo odio qwert dtwudtwu</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
+      <div className="popover__wrapper">
+        <div className="popover__content">
+          <ul className="list-group">
+            <li className="list-group-item">Cras justo odio qwert dtwudtwu</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
           </ul>
         </div>
       </div>
